aa4801eae07543b4a512ce283baf29145645c0cd
IlyaPrigogine <prigogine@protonmail.com>
2023-03-31 14:50:20 +0800

    Update MockA.sol

--- contracts/mock/MockA.sol
+++ contracts/mock/MockA.sol
@@ -1,8 +1,23 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract MockA is ERC20 {
-    constructor(uint256 initialSupply) ERC20("MockA", "MKA") {
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+
+contract MockA is ERC20, ERC20Permit, ERC20Votes {
+    constructor(uint initialSupply) ERC20("MockA", "MKA") ERC20Permit("MockA") {
         _mint(msg.sender, initialSupply);
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 amount) internal virtual override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(address account, uint256 amount) internal virtual override(ERC20, ERC20Votes) {
+        super._mint(account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal virtual override(ERC20, ERC20Votes) {
+        super._burn(account, amount);
     }
 }
